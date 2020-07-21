94816eacb71a3635c3445a1f5c7abb2139b6357f
Pulkit <Pulkitbanta55555@gmail.com>
2020-07-21 15:45:59 +0530

    Added password encryption

--- backend/src/main/java/com/pukitbanta/springblog/config/SecurityConfig.java
+++ backend/src/main/java/com/pukitbanta/springblog/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.pukitbanta.springblog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,4 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 							.authenticated();
 	}
 	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
--- backend/src/main/java/com/pukitbanta/springblog/service/AuthService.java
+++ backend/src/main/java/com/pukitbanta/springblog/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.pukitbanta.springblog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.pukitbanta.springblog.dto.RegisterRequest;
@@ -13,13 +14,22 @@ public class AuthService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public void signup(RegisterRequest registerRequest) {
 		User user = new User();
 		user.setUserName(registerRequest.getUsername());
 		user.setEmail(registerRequest.getEmail());
-		user.setPassword(registerRequest.getPassword());
+		user.setPassword(encodePassword(registerRequest.getPassword()));
 		
 		userRepository.save(user);
 	}
+
+	private String encodePassword(String password) {
+		return passwordEncoder.encode(password);
+	}
+	
+	
 	
 }
