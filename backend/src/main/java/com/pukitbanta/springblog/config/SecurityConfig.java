b224722ee5159cd6dc21ff65c6f03669b46c2f82
Pulkit <Pulkitbanta55555@gmail.com>
2020-07-22 18:53:49 +0530

    added jwt authentication

--- backend/src/main/java/com/pukitbanta/springblog/config/SecurityConfig.java
+++ backend/src/main/java/com/pukitbanta/springblog/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.pukitbanta.springblog.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,12 +12,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.pukitbanta.springblog.security.JwtAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
+	
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter();
+	}
 	
 	@Bean(BeanIds.AUTHENTICATION_MANAGER)
 	@Override
@@ -31,6 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 							.permitAll()
 							.anyRequest()
 							.authenticated();
+		
+		httpSecurity.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	@Autowired
