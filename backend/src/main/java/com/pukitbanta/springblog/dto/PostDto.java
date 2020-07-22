2af91c2f8dfed7b599e9df9a1d61d630732fb43e
Pulkit <Pulkitbanta55555@gmail.com>
2020-07-22 18:58:00 +0530

    added post dto

---
+++ backend/src/main/java/com/pukitbanta/springblog/dto/PostDto.java
@@ -0,0 +1,41 @@
+package com.pukitbanta.springblog.dto;
+
+public class PostDto {
+
+	private Long id;
+	private String content;
+	private String title;
+	private String username;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
