2e8d48941eeb3f26079a3bc1cf125c6bea0022f3
Pulkit <Pulkitbanta55555@gmail.com>
2020-07-27 20:33:42 +0530

    added register request to backend

---
+++ src/app/auth/RegisterRequest.ts
@@ -0,0 +1,6 @@
+export interface RegisterRequest {
+    username: String;
+    email: String;
+    password: String;
+    confirmPassword: String;
+}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { RegisterRequest } from '../RegisterRequest';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -8,33 +10,50 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  signupForm: FormGroup
+  registerForm: FormGroup
   signup = false
   bool = true
   errorText = ""
+  private registerRequest: RegisterRequest;
 
   constructor(
     private fb: FormBuilder,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
-    this.signupForm = this.fb.group({
+    this.registerForm = this.fb.group({
       username: ['', Validators.required],
       email: ['', Validators.required],
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required],
       terms: [false, Validators.required]
     });
+
+    this.registerRequest = {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+
   }
 
   onSubmit() {
-    if (this.signupForm.value.username && this.signupForm.value.email && this.signupForm.value.password && this.signupForm.value.confirmPassword && (this.signupForm.value.password === this.signupForm.value.confirmPassword) && this.signupForm.value.terms) {
-      this.signup = true
+
+    this.registerRequest.username = this.registerForm.get('username').value;
+    this.registerRequest.email = this.registerForm.get('email').value;
+    this.registerRequest.password = this.registerForm.get('password').value;
+    this.registerRequest.confirmPassword = this.registerForm.get('confirmPassword').value;
+
+    this.authService.register(this.registerRequest).subscribe(res => {
       this.bool = true
-    } else {
+      this.signup = true
+    }, error => {
       this.bool = false
       this.signup = false
     }
+    )
   }
 
 }
