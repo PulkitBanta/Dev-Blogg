46926bf034659c8af29c450fe273ebc319fd6822
Pulkit <Pulkitbanta55555@gmail.com>
2020-07-30 21:01:35 +0530

    added asymmetric encryption for jwt token

---
+++ backend/src/main/java/com/pukitbanta/springblog/exception/DevBloggException.java
@@ -0,0 +1,7 @@
+package com.pukitbanta.springblog.exception;
+
+public class DevBloggException extends RuntimeException {
+	public DevBloggException(String message) {
+		super(message);
+	}
+}
--- backend/src/main/java/com/pukitbanta/springblog/security/JwtProvider.java
+++ backend/src/main/java/com/pukitbanta/springblog/security/JwtProvider.java
@@ -1,12 +1,18 @@
 package com.pukitbanta.springblog.security;
 
+import java.io.InputStream;
 import java.security.Key;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
+
+import com.pukitbanta.springblog.exception.DevBloggException;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -16,29 +22,52 @@ import io.jsonwebtoken.security.Keys;
 @Service
 public class JwtProvider {
 	
-	private Key key;
+	private KeyStore keyStore;
 	
 	@PostConstruct
 	public void init() {
-		key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+		try {
+			keyStore = KeyStore.getInstance("JKS");
+			InputStream resourceAsStream = getClass().getResourceAsStream("/dev-blogg.jks");
+		
+			keyStore.load(resourceAsStream, "abc123".toCharArray());	
+		} catch (Exception e) {
+			throw new DevBloggException("Exception occured while loading the keystore");
+		}
 	}
 
 	public String generateToken(Authentication authentication) {
 		User principal = (User) authentication.getPrincipal();
 		return Jwts.builder()
 				.setSubject(principal.getUsername())
-				.signWith(key)
+				.signWith(getPrivateKey())
 				.compact();
 	}
 	
+	private PrivateKey getPrivateKey() {
+		try {
+			return (PrivateKey) keyStore.getKey("dev-blogg", "abc123".toCharArray());
+		} catch (Exception e) {
+			throw new DevBloggException("Exception occured while loading the keystore");
+		}
+	}
+
 	public boolean validateTokent(String jwt) {
-		Jwts.parser().setSigningKey(key).parseClaimsJws(jwt);
+		Jwts.parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
 		return true;
+	}
+
+	private PublicKey getPublicKey() {
+		try {
+			return keyStore.getCertificate("dev-blogg").getPublicKey();
+		} catch (Exception e) {
+			throw new DevBloggException("Exception occured while loading the keystore");
+		}
 	}
 
 	public String getUsernameFromJWT(String token) {
 		Claims claims = Jwts.parser()
-				.setSigningKey(key)
+				.setSigningKey(getPublicKey())
 				.parseClaimsJws(token)
 				.getBody();
 		
