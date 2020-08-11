fd1155640a064973e279294df1a6f371929ae950
Pulkit <Pulkitbanta55555@gmail.com>
2020-08-11 18:27:55 +0530

    updated file name

---
+++ backend/src/main/java/com/pukitbanta/springblog/model/User.java
@@ -0,0 +1,58 @@
+package com.pukitbanta.springblog.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+	private String email;
+
+	@Column
+	private String userName;
+	
+	@Column
+	private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
