84069515521b5215495786ba6164077d0c991487
Pulkit <Pulkitbanta55555@gmail.com>
2020-08-12 16:28:04 +0530

    added custom method to find by user

--- backend/src/main/java/com/pukitbanta/springblog/repository/PostRepository.java
+++ backend/src/main/java/com/pukitbanta/springblog/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.pukitbanta.springblog.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pukitbanta.springblog.model.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long>{
+	
+	List<Post> findByUsername(String username);
 
 }
