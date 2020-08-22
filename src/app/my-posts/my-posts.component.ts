059b8f05ebd7d72408d3643ff113d5bedc571e41
Pulkit <Pulkitbanta55555@gmail.com>
2020-08-22 18:36:54 +0530

    routing component after successful deletion

--- src/app/my-posts/my-posts.component.ts
+++ src/app/my-posts/my-posts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../post.service';
 import { PostRequest } from '../create-post/PostRequest';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-posts',
@@ -12,7 +13,8 @@ export class MyPostsComponent implements OnInit {
   posts$: PostRequest[] = []
 
   constructor(
-    private postService: PostService
+    private postService: PostService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -23,8 +25,10 @@ export class MyPostsComponent implements OnInit {
 
   deletePost(id: number) {
     this.postService.deletePost(id).subscribe(res => {
-      console.log(res);
-      console.log("method called")
+      console.log(res)
+      setTimeout(() => {
+        this.router.navigateByUrl("/home")
+      }, 500);
     }, error => {
       console.log(error);
     })
