963b9da749c2699f678a5af8f061aa295e081694
Pulkit <Pulkitbanta55555@gmail.com>
2020-11-08 15:33:05 +0530

    added serial id

--- backend/src/main/java/com/pukitbanta/springblog/exception/DevBloggException.java
+++ backend/src/main/java/com/pukitbanta/springblog/exception/DevBloggException.java
@@ -1,6 +1,11 @@
 package com.pukitbanta.springblog.exception;
 
 public class DevBloggException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public DevBloggException(String message) {
 		super(message);
 	}
--- backend/src/main/java/com/pukitbanta/springblog/exception/PostNotFoundException.java
+++ backend/src/main/java/com/pukitbanta/springblog/exception/PostNotFoundException.java
@@ -1,6 +1,11 @@
 package com.pukitbanta.springblog.exception;
 
 public class PostNotFoundException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public PostNotFoundException(String message) {
 		super(message);
 	}
